e682f369bfa4c7b590819115f264453bdb2fb4a3
Bui Dai Nghia <6451071050@st.utc2.edu.vn>
2025-11-12 22:41:28 +0700

    up cart-controller

---
+++ Controllers/Client/CartController.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Fastkart.Controllers.Client
+{
+    [Route("/cart")]
+    public class CartController : Controller
+    {
+        [HttpGet("")]
+        public IActionResult Index()
+        {
+            return View("~/Views/Cart/Index.cshtml");
+        }
+    }
+}
