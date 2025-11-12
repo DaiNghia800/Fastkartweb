99d36a767ebd4b8d4228885b8aa65f81c8f44bba
Bui Dai Nghia <6451071050@st.utc2.edu.vn>
2025-11-12 15:35:26 +0700

    up code tmp

---
+++ Models/Entities/CartItem.cs
@@ -0,0 +1,16 @@
+﻿namespace Fastkart.Models.Entities
+{
+    public class CartItem
+    {
+        public int Uid { get; set; }
+
+        public int CartUid { get; set; }
+        public Cart Cart { get; set; }
+
+        // (!!!) THAY ĐỔI QUAN TRỌNG: Liên kết trực tiếp đến Product Uid
+        public int ProductUid { get; set; }
+        public Product Product { get; set; }
+
+        public int Quantity { get; set; }
+    }
+}
---
+++ Models/Entities/Order.cs
@@ -0,0 +1,20 @@
+﻿namespace Fastkart.Models.Entities
+{
+    public class Order
+    {
+        public int Uid { get; set; }
+
+        public int UserUid { get; set; }
+        public User User { get; set; }
+
+        public DateTime OrderDate { get; set; } = DateTime.UtcNow;
+        public decimal TotalAmount { get; set; }
+        public string ShippingAddress { get; set; }
+        public string Status { get; set; } // (Pending, Processing, Shipped...)
+
+        public ICollection<OrderItem> OrderItems { get; set; } = new List<OrderItem>();
+
+        // Một đơn hàng có thể có nhiều thanh toán (hoặc 1)
+        public ICollection<Payment> Payments { get; set; } = new List<Payment>();
+    }
+}
